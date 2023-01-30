2dc7f56890c4529111ebce8f5df250a7da79e5fc
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-30 14:14:22 -0600

    modified const PORT

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ const notes = require("./db/db.json");
 const fs = require("fs");
 //run express app
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; //25-Ins_Heroku: local or heroku
 
+// Middleware for parsing JSON and urlencoded form data
 //https://expressjs.com/en/5x/api.html#express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //WHEN I open the Note Taker, THEN I am presented with a landing
-//Get route index.html
+//Get route homepage
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 //THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-//Get route notes.html
-app.get("/", (req, res) =>
+//Get route notes page
+app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}🚀`));
